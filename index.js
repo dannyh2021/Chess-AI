fc8b1ecf32d6e1e685f76bd645fc224c64b8eefb
dannyh2021 <32226840+dannyh2021@users.noreply.github.com>
2022-01-13 01:41:43 -0500

    Added and set up some libraries.
    Added chess.js, chessboard.js, jquery, and edited script.js to load a chessboard in the starting position.

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 // grab necessary libraries
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const { prependOnceListener } = require('process');
 
-// web pages
-const homePage = fs.readFileSync('index.html');
-const notFoundPage = fs.readFileSync('notfound.html');
+// start new Express app
+const app = express();
 
 // create server
-const server = http.createServer((req, res) => {
-    if(req.url === '/')
-        res.end(homePage);
-    else {
-        res.writeHead(404);
-        res.end(notFoundPage);
-    }
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
 });
 
-server.listen(3000);
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+
+// send resources
+app.get('/css/chessboard-1.0.0.css', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './css/chessboard-1.0.0.css'));
+});
+
+// pictures
+const pieces = ['bB', 'bK', 'bN', 'bP', 'bQ', 'bR', 'wB', 'wK', 'wN', 'wP', 'wQ', 'wR'];
+
+for (let i = 0; i < pieces.length; i++) {
+    app.get('/img/chesspieces/wikipedia/' + pieces[i] + '.png', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './img/chesspieces/wikipedia/' + pieces[i] + '.png'));
+    });
+}
+
+app.get('/js/script.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './js/script.js'));
+    console.log('/img/chesspieces/wikipedia/' + pieces[0] + '.png');
+});
+
+app.get('/js/chessboard-1.0.0.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './js/chessboard-1.0.0.js'));
+});
+
+app.get('/js/chess.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './js/chess.js'))
+});
+
+// send 404 not found page
+/*app.use((req, res) => {
+    res.writeHead(404);
+    res.sendFile(path.resolve(__dirname, 'notFound.html'));
+});*/
---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// board defaults to the starting position when called with no parameters
+// var board, game = new Chess();
+
+// board visualization and game states handling
+
+var cfg = {
+    draggable: true,
+    position: 'start'
+};
+
+// console.log("board: " + board);
+var board1 = Chessboard('board', 'start');
+// board = ChessBoard('board', cfg);
